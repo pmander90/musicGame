922cd670fee3346988f975cc6774827d06b72078
Parmeet Singh <parmeetmander90@gmail.COM>
2015-05-15 10:05:17 -0400

    made changes to js and did bundle exec rake assets:precompile

--- app/assets/javascripts/views/topartist.js
+++ app/assets/javascripts/views/topartist.js
@@ -9,7 +9,7 @@ TopArtistView.prototype = {
 		for(var i=0; i<this.model.topArtists.length; i++){
 			var aTag = $('<a>',{
     			text: this.model.topArtists[i],
-    			href: 'helllllo' + this.model.topArtists[i]
+    			href: 'https://guessthelyrics.herokuapp.com/show/?artist=' + this.model.topArtists[i]
 			});
 			newDiv = $('<div></div>').append(aTag);
 			this.container.append(newDiv);
