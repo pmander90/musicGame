11de5fac0b70d70a43330ca2cb32c8694b461d3b
Parmeet Singh <parmeetmander90@gmail.COM>
2015-07-14 13:18:19 -0400

    added error handling in game controller for songLines

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -18,37 +18,45 @@ class GameController < ApplicationController
 
 		@artist = params[:artist]
 		Genius.access_token= ENV['genius_access_token']
-		# Genius.text_format= 'html'
 		fetcher = Lyricfy::Fetcher.new
-
 		RapGenius::Client.access_token = ENV['genius_access_token']
-		# max artist id = 87014
-		random = rand(19)
-		@name = RapGenius.search_by_artist(@artist)[0].artist.name
+
+		@length = RapGenius.search_by_artist(@artist).length
+		random = rand(@length-1)
+		@name = RapGenius.search_by_artist(@artist)[random].artist.name
 
 		@song0 = RapGenius.search_by_artist(@artist)[random].title
-		@song1 = RapGenius.search_by_artist(@artist)[rand(19)].title
-		@song2 = RapGenius.search_by_artist(@artist)[rand(19)].title
-		@song3 = RapGenius.search_by_artist(@artist)[rand(19)].title
+		@song1 = RapGenius.search_by_artist(@artist)[rand(@length-1)].title
+		@song2 = RapGenius.search_by_artist(@artist)[rand(@length-1)].title
+		@song3 = RapGenius.search_by_artist(@artist)[rand(@length-1)].title
 		@songs = [@song0,@song1,@song2,@song3].shuffle
 		
 		@id = RapGenius.search_by_artist(@artist)[random].id
 
 		@media = Genius::Song.find(@id).media[0]
-		if @media['provider'] == "youtube"
-			@works = true
-			@videoId = @media['url'].split("=").last
-		else
+		if @media != nil
+			if @media['provider'] == "youtube"
+				@works = true
+				@videoId = @media['url'].split("=").last
+			else
+				@works = false
+			end
+		else 
 			@works = false
 		end
 
-		songLines = fetcher.search(@artist, @song0)
-		numLines = songLines.lines.length-1
-		random2 = rand(0...numLines)
-		@lyric1 = songLines.lines[random2]
-		while @lyric1 == "" || @lyric1.split().length < 5
-			random2 = rand(0...numLines) 
+		begin
+			songLines = fetcher.search(@name, @song0)
+		rescue
+			@error = true
+		else
+			numLines = songLines.lines.length-1
+			random2 = rand(0...numLines)
 			@lyric1 = songLines.lines[random2]
+			while @lyric1 == "" || @lyric1.split().length < 5
+				random2 = rand(0...numLines) 
+				@lyric1 = songLines.lines[random2]
+			end
 		end
 	end
 
